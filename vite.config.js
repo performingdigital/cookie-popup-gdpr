5ef3e0b9b79ced8fcd9ffd7c12856ea3488affdd
Giorgio Pogliani <giorgio.pogliani@performingdigital.com>
2022-01-23 22:39:50 +0100

    update configs

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/main.js"),
+      name: "cookie-popup-gdpr",
+      fileName: (format) => `cookie-popup-gdpr.${format}.js`,
+    },
     target: "esnext",
     assetsInlineLimit: 100000000,
     chunkSizeWarningLimit: 100000000,
